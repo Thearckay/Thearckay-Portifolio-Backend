6d3707849ceef4026eadc4a2fd28c780b21ec809
thearckay <thearckay@gmail.com>
2026-03-16 16:45:46 -0300

    Configurando CORS para funcionamento das requisições no ambiente de produção

--- src/main/java/com/thearckay/portifolio/config/security/SecurityConfig.java
+++ src/main/java/com/thearckay/portifolio/config/security/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://localhost:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("https://thearckay-portifolio-backend.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
